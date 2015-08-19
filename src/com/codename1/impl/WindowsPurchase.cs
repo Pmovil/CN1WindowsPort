913f70b853bee4deba4b98696b6442a38c367368
Fabricio <fabricio@pmovil.com.br>
2015-08-19 16:40:26 -0300

    adding overrides

--- src/com/codename1/impl/WindowsPurchase.cs
+++ src/com/codename1/impl/WindowsPurchase.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace com.codename1.impl
 {
-    class WindowsPurchase
+    public class WindowsPurchase : com.codename1.payment.Purchase
     {
+    
+        public override bool isManagedPaymentSupported()
+        {
+            return base.isManagedPaymentSupported();
+        }
+        public override bool wasPurchased(java.lang.String n1)
+        {
+            return base.wasPurchased(n1);
+        }
+        public override object getProducts(org.xmlvm._nArrayAdapter<object> n1)
+        {
+            return base.getProducts(n1);
+        }
     }
 }
